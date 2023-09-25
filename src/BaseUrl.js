245cc471e6dd6fdec2889cd9ed87699c2960ad63
Raj Chavda <89194891+Rajtheappideas@users.noreply.github.com>
2023-09-25 09:49:15 +0000

    base url change

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -2,10 +2,11 @@ import axios from "axios";
 
 const language = JSON.parse(window.localStorage.getItem("lang"));
 
-export default axios.defaults.baseURL = "http://15.235.192.7:3000";
+// export default axios.defaults.baseURL = "http://15.235.192.7:3000";
+export default axios.defaults.baseURL = "https://boisnewsmedia.onrender.com";
 
 export const PostUrl = axios.create({
-  baseURL: "http://15.235.192.7:3000/api/admin",
+  baseURL: "https://boisnewsmedia.onrender.com/api/admin",
   method: "POST",
   headers: {
     "Content-Type": "Application/json",
@@ -14,7 +15,7 @@ export const PostUrl = axios.create({
 });
 
 export const GetUrl = axios.create({
-  baseURL: "http://15.235.192.7:3000/api/admin",
+  baseURL: "https://boisnewsmedia.onrender.com/api/admin",
   method: "GET",
   headers: {
     "Content-Type": "Application/json",
